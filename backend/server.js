df6bcedfac832455ad5b469b934879c61283254a
GoatProgrammer <infernodia@gmail.com>
2021-06-02 12:16:54 -0400

    added comments and some other changes I forgot about

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,23 @@ app.use(express.json())
 app.use(cors())
 
 // routes
-app.get('/', (req, res) => {
-  res.send("api is running")
-})
+
 
 app.use('/api/projects', projectRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/projects/delete', projectRoutes)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send("api is running")
+  })
+}
 
 
 const PORT = process.env.PORT || 9000
